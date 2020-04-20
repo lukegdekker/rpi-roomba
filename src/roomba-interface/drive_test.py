c3b25e90f5efa8be1fcbe15b5862832313485a38
lukegdekker <luke.g.dekker@gmail.com>
2020-04-20 10:25:42 -0400

    added joy import

--- src/roomba-interface/drive_test.py
+++ src/roomba-interface/drive_test.py
@@ -3,12 +3,13 @@ import time
 from pyroombaadapter import PyRoombaAdapter
 import numpy as np
 import rospy
+from sensor_msgs.msg import Joy
 
 PORT = "/dev/ttyS0"
 
 class RoombaDrive:
     def __init__(self):
-        self.roomba = PyRoombaAdapter(PORT)
+        #self.roomba = PyRoombaAdapter(PORT)
         self.cmdSub = rospy.Subscriber("/joy", Joy, self.getCmd)
 
     def getCmd(self, cmd):
