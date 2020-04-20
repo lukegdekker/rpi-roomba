91a5dd21c724994e7374c2f51bd0bc5d63170811
Lukas Dekker <ldekker@macleanengineering.com>
2020-04-20 09:40:58 -0400

    added drive test code

--- src/roomba-interface/drive_test.py
+++ src/roomba-interface/drive_test.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+import time
+from pyroombaadapter import PyRoombaAdapter
+import numpy as np
+import rospy
+
+PORT = "/dev/ttyS0"
+
+class RoombaDrive:
+    def __init__(self):
+        self.roomba = PyRoombaAdapter(PORT)
+        self.cmdSub = rospy.Subscriber("/joy", Joy, self.getCmd)
+
+    def getCmd(self, cmd):
+        print(cmd)
+
+if __name__ == '__main__':
+    rospy.init_node("roomba_interface")
+    interface = RoombaDrive()
+    while True:
+        try:
+            rospy.spin()
+        except KeyboardInterrupt:
+            exit()
+            
+            
+
+    
